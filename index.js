8ea9ac79f47492d6ea1b764ebbcbbee96f294e39
hoffm386 <erinrhof@uw.edu>
2019-06-11 16:36:30 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const winObj = record.find( (recordObj) => recordObj.result == "W");
+  if (winObj) {
+    return winObj.year;
+  } else {
+    return winObj;
+  }
+
+}
